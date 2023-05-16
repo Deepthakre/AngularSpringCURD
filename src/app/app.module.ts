092dc183e601058c6029f599daafc290fe157ec6
deep <thakaredeep43@gmail.com>
2023-05-16 12:04:01 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import { HomeComponent } from './home/home.component';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+import { FormsModule } from '@angular/forms';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    EmployeeListComponent,
+    CreateEmployeeComponent,
+    UpdateEmployeeComponent,
+    EmployeeDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/employee.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Employee } from 'src/employee';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  private baseUrl="http://localhost:8080/api/v1/employee";
+
+  constructor(private httpClient:HttpClient) { }
+
+  //get employe data
+
+  getEmployeeList():Observable<Employee[]>{
+    return this.httpClient.get<Employee[]>(`${this.baseUrl}`);
+  }
+
+  //create employee
+
+  createEmployee(employee:Employee):Observable<object>{
+    return this.httpClient.post(`${this.baseUrl}`,employee);
+  }
+
+  //get employee by id
+
+  getEmployeeById(id:number):Observable<Employee>{
+    return this.httpClient.get<Employee>(`${this.baseUrl}/${id}`);
+
+  }
+  //update employee
+  updateEmployee(id:number,employee:Employee):Observable<object>{
+    return this.httpClient.put(`${this.baseUrl}/${id}`,employee);
+
+  }
+  //delete employee
+  
+  deleteEmployee(id:number):Observable<object>{
+     return this.httpClient.delete(`${this.baseUrl}/${id}`);
+  }
+
+}
